f5a7d8e99dc89994ba5adc2bf3db96b9f344157e
Dor Bar <dor4231@gmail.com>
2018-05-20 23:56:01 +0300

    Phntom and eslint added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
+var jasmine = require('gulp-jasmine-phantom');
 
-gulp.task('default', function() {
-	console.log('hello world!');
+gulp.task('default', ["styles", "lint"], function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['lint']);
+
+	browserSync.init({
+		server: './'
+	});
 });
 
 gulp.task('styles', function() {
@@ -14,6 +21,27 @@ gulp.task('styles', function() {
 			browsers: ['last 2 versions']
 		}))
 		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('lint', function () {
+	return gulp.src(['js/**/*.js'])
+		// eslint() attaches the lint output to the eslint property
+		// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failOnError last.
+		.pipe(eslint.failOnError());
+});
+
+gulp.task('tests', function () {
+	gulp.src('tests/spec/extraSpec.js')
+		.pipe(jasmine({
+			integration: true,
+			vendor: 'js/**/*.js'
+		}));
 });
 
 // Static server
